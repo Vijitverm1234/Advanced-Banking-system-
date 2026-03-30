447fcfd33cf2b69b7c840a4c1197dda112d07f6e
Vijitverm1234 <vijitverma78@gmail.com>
2026-03-30 14:32:21 +0530

    feat(backend): UserModel Implementation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,11 @@
 const mongoose=require('mongoose')      
+const userSchema=mongoose.Schema({
+    email:{
+        type:String,
+        required:[true,"Email is required"],
+        trim:true,
+        lowercase:true,
+        match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,'Please fill a valid email address']
+        
+    }
+})
